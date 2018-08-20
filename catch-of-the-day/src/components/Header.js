091b6244414906e5142458f7a1c3b496b9b4451b
Shaun Luttin <shaunluttin@gmail.com>
2018-08-20 15:43:54 -0700

    Complete video 07

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -14,7 +14,7 @@ class Header extends React.Component {
                 </h1>
                 <h3 className="tagline">
                     <span>
-                        Fresh Daily
+                        {this.props.tagline}
                     </span>
                 </h3>
             </header>
