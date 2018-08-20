6a7a96ad0cccc7781c5ffb8124312ab9afed7017
Shaun Luttin <shaunluttin@gmail.com>
2018-08-20 12:06:56 -0700

    Complete video 03 exercises

---
+++ catch-of-the-day/src/components/FishStore.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class FishStore extends React.Component {
+    render() {
+        return <p>Hello world</p>
+    }
+}
+
+export default FishStore;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import FishStore from './components/FishStore';
+
+render(
+    <FishStore />,
+    document.querySelector('#main'));
