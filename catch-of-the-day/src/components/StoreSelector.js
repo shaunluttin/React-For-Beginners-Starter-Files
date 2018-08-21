d35545217bb7f8d6866f5a34a2469b6741ebd3a4
Shaun Luttin <shaunluttin@gmail.com>
2018-08-21 10:46:11 -0700

    Complete video 11

--- catch-of-the-day/src/components/StoreSelector.js
+++ catch-of-the-day/src/components/StoreSelector.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 class StoreSelector extends React.Component {
+
+    myInput = React.createRef();
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Submitted ${this.myInput}`);
+    }
+
     render() {
         return (
-            <form className='store-selector'>
+            <form className='store-selector' onSubmit={this.handleSubmit}>
                 <h2>Please enter a store:</h2>
-                <input type='text' required placeholder="Input store:"></input>
+                <input type='text' required placeholder="Input store:" ref={this.myInput}></input>
                 <button type='submit'>Visit store</button>
             </form>
         );
